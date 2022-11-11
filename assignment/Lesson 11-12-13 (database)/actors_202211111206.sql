44800a4f359f19430b0c46cf2b1866138d862569
farbantis <79906544+farbantis@users.noreply.github.com>
2022-11-11 13:16:39 +0300

    Add files via upload

---
+++ assignment/Lesson 11-12-13 (database)/actors_202211111206.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.actors (name,film_id) VALUES
+	 ('John Smith',1),
+	 ('John Smith',5),
+	 ('John Smith',10),
+	 ('Andrew Stown',1),
+	 ('Andrew Stone',3),
+	 ('Andrew Stone',10),
+	 ('Scot Flaw',1),
+	 ('Scot Flaw',3),
+	 ('Scot Flaw',8),
+	 ('Scot Flow',10);
+INSERT INTO public.actors (name,film_id) VALUES
+	 ('Billy Johnes',1),
+	 ('Billy Johnes',3);
---
+++ assignment/Lesson 11-12-13 (database)/movies_202211111206.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.movies (name,"year",country,budget) VALUES
+	 ('Baby Bulldog',2005,'USA',1000),
+	 ('Barbie: Mermaid Power',2005,'USA',1000),
+	 ('Camp Manna',2005,'USA',1000),
+	 ('Chain Reaction',2006,'USA',1000),
+	 ('Dahmer',2005,'USA',1000),
+	 ('Face of a Fugitive',2006,'USA',1000),
+	 ('French Twist',2005,'USA',1000),
+	 ('Getting Grace',2006,'USA',1000),
+	 ('Here Out West',2008,'USA',1000),
+	 ('Back to the Beach',2006,'Canada',1000);
+INSERT INTO public.movies (name,"year",country,budget) VALUES
+	 ('Coming Apart',2008,'Canada',1000),
+	 ('Final Justice',2005,'Spain',1000);
